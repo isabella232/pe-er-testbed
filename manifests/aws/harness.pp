934c36fdb7dca130950064d001c6a5207ae2522a
Nathan Valentine <nathan@puppetlabs.com>
2014-02-10 12:36:22 -0800

    start hacking on provisioning of AWS

--- manifests/aws/harness.pp
+++ manifests/aws/harness.pp
@@ -1,4 +1,25 @@
 class testbed::aws::harness (
+  $projects = {},
+  $aws_access_key_id,
+  $aws_secret_access_key,
+  $aws_ssh_pub_key,
+  $aws_ssh_priv_key
 ) inherits testbed::aws {
+
+  validate_hash($projects)
+  validate_string($aws_access_key_id)
+  validate_string($aws_secret_access_key)
+  validate_string($aws_ssh_pub_key)
+  validate_string($aws_ssh_priv_key)
+
   file { '/etc/puppetlabs/puppet/nodes/aws': ensure => directory, }
+
+  File { owner => 'root', group => 'root', mode => '06', ensure => file, }
+
+  file { 'root fog config':
+    mode => '0600',
+    content => template("${module_name}/fog.erb"),   
+  }
+
+  package { ['ec2-ami-tools', 'ec2-api-tools']: ensure => installed, }
 }
