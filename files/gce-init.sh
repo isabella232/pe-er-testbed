72836d328c957d7e16186599fb79ef30b12d8f81
Nathan Valentine <nathan@puppetlabs.com>
2014-02-10 10:52:12 -0800

    adjust script

--- files/gce-init.sh
+++ files/gce-init.sh
@@ -4,7 +4,7 @@ PATH=$PATH:/opt/google-cloud-sdk/bin
 
 #gcloud auth login
 
-for i aio puppetdb mco; do
+for i in aio puppetdb mco; do
   gcloud config set project pe-er-$i && \
   puppet apply /etc/puppetlabs/puppet/environments/production/modules/testbed/tests/testbed-gce-$i.pp \
     --certname $i --debug
