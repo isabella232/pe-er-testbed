49254f74762f264132401cdf60a2fb3b671c04d7
Nathan Valentine <nathan@puppetlabs.com>
2014-02-13 09:46:35 -0800

    refactor gce setup

--- manifests/gce/aio.pp
+++ manifests/gce/aio.pp
@@ -3,33 +3,9 @@ class testbed::gce::aio(
   $firewalls = {},
   $puppetmaster_fqdn,
   $puppetmaster_ip
-) {
+) inherits testbed::gce {
   validate_hash($instances)
   validate_hash($firewalls)
   validate_string($puppetmaster_fqdn)
   validate_string($puppetmaster_ip)
-
-  require ::testbed::gce
-
-  $instances_defaults = {
-    'ensure' => 'present',
-    'zone' => 'us-central1-a', 
-    'network' => 'default',
-    'image' => 'projects/centos-cloud/global/images/centos-6-v20131120',
-    'startupscript' => 'puppet-enterprise.sh',
-    'metadata' => {
-      'pe_role' => 'agent',
-      'pe_master' => 'puppet',
-      'pe_version' => '3.1.3'
-    },
-    'manifest' => "host { ${puppetmaster_fqdn}:
-        ensure => present,
-        ip => ${puppmaster_ip},
-        aliases => 'puppet',
-        }"
-    }
-  create_resources('gce_instance', $instances, $instances_defaults)
-
-  $firewalls_defaults = { 'ensure' => 'present', 'network' => 'default', }
-  create_resources('gce_firewall', $firewalls, $firewalls_defaults)
 }
--- manifests/gce/puppetdb.pp
+++ manifests/gce/puppetdb.pp
@@ -1,29 +1,11 @@
 class testbed::gce::puppetdb(
   $instances = {},
-  $firewalls = {}
-) {
+  $firewalls = {},
+  $puppetmaster_fqdn,
+  $puppetmaster_ip
+) inherits testbed::gce {
   validate_hash($instances)
   validate_hash($firewalls)
-
-  require ::testbed::gce
-
-  $instances_defaults = {
-    'ensure' => 'present',
-    'zone' => 'us-central1-a', 
-    'network' => 'default',
-    'image' => 'projects/centos-cloud/global/images/centos-6-v20131120',
-    'startupscript' => 'puppet-enterprise.sh',
-    'metadata' => {
-      'pe_role' => 'agent',
-      'pe_master' => 'puppet',
-       'pe_version' => '3.1.3'
-    }
-  }
-  create_resources('gce_instance', $instances, $instances_defaults)
-
-  $firewalls_defaults = {
-    'ensure' => 'present',
-    'network' => 'default',
-  }
-  create_resources('gce_firewall', $firewalls, $firewalls_defaults)
+  validate_string($puppetmaster_fqdn)
+  validate_string($puppetmaster_ip)
 }
