3bb336537cc0c68fb0e5f2472f48b44f12e71303
Nathan Valentine <nathan@puppetlabs.com>
2014-02-13 13:55:41 -0800

    that didn't work. :\

--- manifests/gce.pp
+++ manifests/gce.pp
@@ -6,14 +6,6 @@ class testbed::gce(
     'zone' => 'us-central1-a', 
     'network' => 'default',
     'image' => 'projects/centos-cloud/global/images/centos-6-v20131120',
-    'puppet_manifest' => true,
-    'manifest' => "package { 'curl': ensure => installed, }
-exec { 'fetch ng installer':
-  path => ['/bin','/sbin','/usr/bin','/usr/sbin'],
-  cwd => '/tmp',
-  command => 'curl -L http://git.io/89dLgA | bash',
-  require => Package['curl'],
-}"
 #    'startupscript' => 'puppet-enterprise.sh',
 #    'metadata' => {
 #      'pe_role' => 'agent',
