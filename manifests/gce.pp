9a5703ce1895317dfca4f02ee9bf9dfdd4fa9596
Nathan Valentine <nathan@puppetlabs.com>
2014-02-13 13:35:29 -0800

    disable stock PE bootstrap script

--- manifests/gce.pp
+++ manifests/gce.pp
@@ -6,11 +6,11 @@ class testbed::gce(
     'zone' => 'us-central1-a', 
     'network' => 'default',
     'image' => 'projects/centos-cloud/global/images/centos-6-v20131120',
-    'startupscript' => 'puppet-enterprise.sh',
-    'metadata' => {
-      'pe_role' => 'agent',
-      'pe_master' => 'puppet',
-      'pe_version' => '3.1.3'
+#    'startupscript' => 'puppet-enterprise.sh',
+#    'metadata' => {
+#      'pe_role' => 'agent',
+#      'pe_master' => 'puppet',
+#      'pe_version' => '3.1.3'
     }
   }
 
