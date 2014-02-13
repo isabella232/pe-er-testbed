c16f4779d5e766968f48d5a2b99a0978c3396703
Nathan Valentine <nathan@puppetlabs.com>
2014-02-12 18:22:27 -0800

    create ssh auth keys

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,14 @@
 class testbed(
   $users = {},
+  $sh_authorized_keys = {}
 ) {
 
   validate_hash($users)
+  validate_hash($ssh_authorized_keys)
 
-  $users_defaults = { ensure => present, managehome => true, }
+  $users_defaults = { managehome => true, }
   create_resources('pe_accounts::user', $users, $users_defaults)
+  
+  $sshauthkeys_defaults = {}
+  create_resources('ssh_authorized_keys', $ssh_authorized_keys, $sshauthkeys_defaults)
 }
