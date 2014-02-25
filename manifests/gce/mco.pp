f2730a5a254de6294dcedf2c70517d3301fbbe2a
Nathan Valentine <nathan@puppetlabs.com>
2014-02-24 16:16:42 -0800

    add ability to create disks to GCE envs

--- manifests/gce/mco.pp
+++ manifests/gce/mco.pp
@@ -1,14 +1,25 @@
 class testbed::gce::mco(
+  $disks = {},
   $instances = {},
-  $firewalls = {},
-  $puppetmaster_fqdn,
-  $puppetmaster_ip
+  $firewalls = {}
 ) inherits testbed::gce {
+  validate_hash($disks)
   validate_hash($instances)
   validate_hash($firewalls)
-  validate_string($puppetmaster_fqdn)
-  validate_string($puppetmaster_ip)
 
+  require ::testbed::gce
+
+  $disks_defaults = { 'ensure' => 'present', 'zone' => 'us-central1-a', }
+  create_resources('gce_disk', $disks, $disks_defaults)
+
+  $instances_defaults = {
+    'ensure' => 'present',
+    'zone' => 'us-central1-a', 
+    'network' => 'default',
+    'image' => 'projects/centos-cloud/global/images/centos-6-v20131120',
+  }
   create_resources('gce_instance', $instances, $instances_defaults)
+
+  $firewalls_defaults = { 'ensure' => 'present', 'network' => 'default', }
   create_resources('gce_firewall', $firewalls, $firewalls_defaults)
 }
