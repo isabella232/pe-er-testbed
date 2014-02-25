43c6ee08d52385e190220e0d2174a4a4f378e0ed
Nathan Valentine <nathan@puppetlabs.com>
2014-02-24 16:42:16 -0800

    empty out testbed::gce to stub

--- manifests/gce.pp
+++ manifests/gce.pp
@@ -1,18 +1,4 @@
 class testbed::gce(
 
 ) {
-  $instances_defaults = {
-    'ensure' => 'present',
-    'zone' => 'us-central1-a', 
-    'network' => 'default',
-    'image' => 'projects/centos-cloud/global/images/centos-6-v20131120',
-#    'startupscript' => 'puppet-enterprise.sh',
-#    'metadata' => {
-#      'pe_role' => 'agent',
-#      'pe_master' => 'puppet',
-#      'pe_version' => '3.1.3'
-#    }
-  }
-
-  $firewalls_defaults = { 'ensure' => 'present', 'network' => 'default', }
 }
