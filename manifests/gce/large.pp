aec61be5ddfc6bd0103df16381dc8a537e261890
Nathan Valentine <nathan@puppetlabs.com>
2014-03-04 17:10:27 -0800

    add code for testbed::gce::large

---
+++ manifests/gce/large.pp
@@ -0,0 +1,25 @@
+class testbed::gce::large(
+  $disks = {},
+  $instances = {},
+  $firewalls = {}
+) inherits testbed::gce {
+  validate_hash($disks)
+  validate_hash($instances)
+  validate_hash($firewalls)
+
+  require ::testbed::gce
+
+  $disks_defaults = { ensure => 'present', zone => 'us-central1-a', source_image => 'centos-6-v20131120', }
+  create_resources('gce_disk', $disks, $disks_defaults)
+
+  $instances_defaults = {
+    'ensure' => 'present',
+    'zone' => 'us-central1-a', 
+    'network' => 'default',
+    'image' => 'projects/centos-cloud/global/images/centos-6-v20131120',
+  }
+  create_resources('gce_instance', $instances, $instances_defaults)
+
+  $firewalls_defaults = { 'ensure' => 'present', 'network' => 'default', }
+  create_resources('gce_firewall', $firewalls, $firewalls_defaults)
+}
